d0df5187a6f9a05ab6469746b65d9b410ae7b07d
Sujon Hossain <sujon.hossain758@gmail.com>
2021-11-15 01:33:41 +0600

    remove server this project and create sperate server with express js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,34 +6,48 @@ import axios from 'axios';
 import { GetServerSideProps, NextPage } from 'next';
 import { useRouter } from 'next/dist/client/router';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { getAsString } from 'src/utils/getAsString';
 
 interface Props {
-    success: boolean;
     data: {
         _id: string;
         make: string;
         count: number;
     }[];
-    message: string;
+    make: {
+        _id: string;
+        model: string;
+    }[];
 }
 interface IFormInputs {
     make: string;
     model: string;
+    maxPrice: string;
+    minPrice: string;
 }
+const prices = [
+    500, 1000, 2500, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000,
+    45000, 50000,
+];
 
-const Home: NextPage<Props> = ({ data }: Props) => {
+const Home: NextPage<Props> = ({ data, make }: Props) => {
     const { query } = useRouter();
+    const { handleSubmit, control, watch } = useForm<IFormInputs>();
 
-    const initialValues: IFormInputs = {
-        make: (query.make || 'all') as string,
-        model: query.model as string,
+    const initialValues = {
+        make: getAsString(query.make as string | string[]) || 'all',
+        model: getAsString(query.model as string | string[]) || 'all',
+        minPrice: getAsString(query.minPrice as string | string[]) || 'all',
+        maxPrice: getAsString(query.maxPrice as string | string[]) || 'all',
     };
 
-    const { handleSubmit, control } = useForm<IFormInputs>();
+    console.log(watch('make'));
+
     const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log(data);
 
     return (
         <Container maxWidth="lg">
+            <pre> {JSON.stringify(make, null, 4)} </pre>
             <Paper sx={{ marginTop: '30px', padding: '10px' }} elevation={2}>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <Grid container spacing={3}>
@@ -91,6 +105,60 @@ const Home: NextPage<Props> = ({ data }: Props) => {
                                 />
                             </FormControl>
                         </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">
+                                    Min Price
+                                </InputLabel>
+                                <Controller
+                                    name="minPrice"
+                                    control={control}
+                                    defaultValue={initialValues.minPrice}
+                                    render={({ field }) => (
+                                        <Select {...field} label="Make">
+                                            <MenuItem value="all">
+                                                No Min
+                                            </MenuItem>
+                                            {prices.map((price) => (
+                                                <MenuItem
+                                                    value={price}
+                                                    key={price}
+                                                >
+                                                    {price}
+                                                </MenuItem>
+                                            ))}
+                                        </Select>
+                                    )}
+                                />
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">
+                                    Max Price
+                                </InputLabel>
+                                <Controller
+                                    name="maxPrice"
+                                    control={control}
+                                    defaultValue={initialValues.maxPrice}
+                                    render={({ field }) => (
+                                        <Select {...field} label="Max Price">
+                                            <MenuItem value="all">
+                                                No Max
+                                            </MenuItem>
+                                            {prices.map((price) => (
+                                                <MenuItem
+                                                    value={price}
+                                                    key={price}
+                                                >
+                                                    {price}
+                                                </MenuItem>
+                                            ))}
+                                        </Select>
+                                    )}
+                                />
+                            </FormControl>
+                        </Grid>
                     </Grid>
                     <button type="submit">Submit</button>
                 </form>
@@ -99,11 +167,17 @@ const Home: NextPage<Props> = ({ data }: Props) => {
     );
 };
 
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
     const { data } = await axios.get('http://localhost:3000/api/cars/make');
+    const make = getAsString(ctx.query.make as string | string[]);
+
+    const { data: model } = await axios.get(
+        `http://localhost:3000/api/make/${make}`
+    );
     return {
         props: {
-            ...data,
+            data: data.data,
+            make: model,
         },
     };
 };
